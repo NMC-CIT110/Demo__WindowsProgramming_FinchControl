df7595abc666bb0dc2a471a95f2c3594083d7436
John Velis <john@johnvelis.com>
2015-12-08 15:35:28 -0500

    initial commit

---
+++ Demo__Windows_FinchControl/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Demo__Windows_FinchControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFoward = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFoward
+            // 
+            this.btnFoward.Location = new System.Drawing.Point(185, 101);
+            this.btnFoward.Name = "btnFoward";
+            this.btnFoward.Size = new System.Drawing.Size(75, 23);
+            this.btnFoward.TabIndex = 0;
+            this.btnFoward.Text = "Forward";
+            this.btnFoward.UseVisualStyleBackColor = true;
+            this.btnFoward.Click += new System.EventHandler(this.btnFoward_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 388);
+            this.Controls.Add(this.btnFoward);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFoward;
+    }
+}
+
---
+++ Demo__Windows_FinchControl/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FinchAPI;
+
+namespace Demo__Windows_FinchControl
+{
+    public partial class Form1 : Form
+    {
+        Finch _myFinch;
+        int _speed;
+
+
+        public Form1()
+        {
+            Finch myFinch = new Finch();
+            myFinch.connect();
+
+            InitializeComponent();
+        }
+
+        private void btnFoward_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
